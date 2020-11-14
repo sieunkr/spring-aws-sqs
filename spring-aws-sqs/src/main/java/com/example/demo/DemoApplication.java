cd215fdbc3e8520f97c184f486011d83f8b6c06c
sieunkim <sieunkr@gmail.com>
2020-11-14 19:04:48 +0900

    sqs 큐 생성

---
+++ spring-aws-sqs/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.AmazonSQSException;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+        CreateQueueRequest create_request = new CreateQueueRequest("my-queue")
+                .addAttributesEntry("DelaySeconds", "60")
+                .addAttributesEntry("MessageRetentionPeriod", "86400");
+
+        try {
+            sqs.createQueue(create_request);
+        } catch (AmazonSQSException e) {
+            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+                throw e;
+            }
+        }
+    }
+}
